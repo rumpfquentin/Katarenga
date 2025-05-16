94ecbe38399daee0c6ad2efebd4961b1bed6b3a3
Rumpfquentin <quentin.rumpf@gmail.com>
2025-05-16 12:08:14 +0100

    inserting puzzle.txt

--- helloNEA.py
+++ helloNEA.py
@@ -1,1 +1,9 @@
-print('Hello, NEA!')
+file = open('r', "Puzzles.txt.rtf")
+print(file)
+class Event:
+    def __init__(self):
+        pass
+
+class Puzzle:
+    def __init__(self):
+        self.shape() 
