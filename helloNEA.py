e3288d86a7be0da591ed81a448e0f3f9533b223e
Rumpfquentin <quentin.rumpf@gmail.com>
2025-05-16 20:13:47 +0100

    debugging piece and puzzles

--- helloNEA.py
+++ helloNEA.py
@@ -1,16 +1,20 @@
 Whitestack = []
 Blackstack = []
-
+piece_objects = []
 with open("Puzzles.txt.rtf") as file:
     puzzles = (file.readlines())
-
+with open("Pieces.txt") as file:
+    pieces = (file.readlines())
 class Event:
     def __init__(self):
         pass
-class
+class Piece:
+    def __init__(self, ID):
+        self.shape = [ch for ch in ID if ch.upper() in ('X', '1')]
+        self.type = ID[1]
 class Puzzle:
     def __init__(self, ID):
-        self.shape = [chr.upper() for chr in ID if chr.upper() in ("0", "X")]
+        self.shape = [ch.upper() for ch in ID if ch.upper() in ("0", "X")]
         self.type = ID[1]
 
 
@@ -21,6 +25,8 @@ for puzzleID in puzzles:
         Whitestack.append(puzzle)
     elif puzzle.type == "B":
         Blackstack.append(puzzle)
+for pid in pieces:
+    piece_objects.append(Piece(pid))
 
 [print(puzzle.shape) for puzzle in Whitestack]
-    
+[print(piece.shape) for piece in piece_objects]
