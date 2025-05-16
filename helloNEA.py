370f32733e9d83b0659990f9e6ce1e5834c88357
Rumpfquentin <quentin.rumpf@gmail.com>
2025-05-16 19:57:09 +0100

    Piece implementation

--- helloNEA.py
+++ helloNEA.py
@@ -1,10 +1,26 @@
+Whitestack = []
+Blackstack = []
+
 with open("Puzzles.txt.rtf") as file:
-    print(file.readlines())
-print(file)
+    puzzles = (file.readlines())
+
 class Event:
     def __init__(self):
         pass
-
+class
 class Puzzle:
-    def __init__(self):
-        self.shape() 
+    def __init__(self, ID):
+        self.shape = [chr.upper() for chr in ID if chr.upper() in ("0", "X")]
+        self.type = ID[1]
+
+
+for puzzleID in puzzles:
+    print(puzzleID)
+    puzzle = Puzzle(puzzleID)
+    if puzzle.type == "W":
+        Whitestack.append(puzzle)
+    elif puzzle.type == "B":
+        Blackstack.append(puzzle)
+
+[print(puzzle.shape) for puzzle in Whitestack]
+    
