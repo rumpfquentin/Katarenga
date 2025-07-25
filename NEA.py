1489c6284f6a2c0425368bbb85896c18bb7f9829
Rumpfquentin <quentin.rumpf@gmail.com>
2025-07-25 16:27:45 +0200

    creating game setup

--- NEA.py
+++ NEA.py
@@ -1,3 +1,19 @@
-class Piece():
-    def __init__():
+import random
+
+class Piece:
+    def __init__(self, player,x,y):
+        self.player = player
+        self.posx = x
+        self.posy = y
+    def movepiece():
+        pass
+class Board:
+    def __init__(self):
+        self.layout = []
+    def randomlayout(self):
+        with open('Tiles.txt') as f:
+            x = f.readlines().strip()
+            for i in range(4):
+                x.pop(random.randint(1,8))
         
+
