2d5fb84d28b92d15a64147424d7986990be7df68
Rumpfquentin <quentin.rumpf@gmail.com>
2025-10-14 09:19:04 +0000

    Delete smoke.py

--- smoke.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.core.window import Window
-Window.clearcolor = (0.12, 0.12, 0.12, 1)   # opaque!
-
-class T(App):
-    def build(self):
-        return Label(text="Hello Kivy", font_size="40sp")
-T().run()
